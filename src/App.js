e56c03b54dedad692bb572228d17dbcabc5f99b1
diana.silva <diana.silva@rebase.com.br>
2020-09-08 20:36:07 -0300

    Remove almost components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css'
-import Header from "./components/header/index";
-import Introduction from "./components/introduction/index";
-import Skills from "./components/skills/index";
+//import Header from "./components/header/index";
+//import Introduction from "./components/introduction/index";
+//import Skills from "./components/skills/index";
 //import Education from "./components/education/index";
 //import Bootcamps from "./components/bootcamps/index";
-import Portfolio from "./components/portfolio/index";
+//import Portfolio from "./components/portfolio/index";
 import Footer from "./components/footer/index"
 
 function App() { 
 
 	return (
 		<>
-			<Header/>
-			<Introduction/>
-			<Portfolio/>
-			<Skills/>
 			<Footer/>
 		</>
 	);
