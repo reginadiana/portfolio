6e702fe478bf87ce5735078ad389501664dc9bcc
diana.silva <diana.silva@rebase.com.br>
2020-09-08 20:28:01 -0300

    Remove unecessary components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import './App.css'
 import Header from "./components/header/index";
 import Introduction from "./components/introduction/index";
-import Expertise from "./components/expertise/index";
 import Skills from "./components/skills/index";
-import Lenguages from "./components/lenguages/index"
 import Education from "./components/education/index";
 import Bootcamps from "./components/bootcamps/index";
-import Profiles from "./components/profiles/index";
 import Portfolio from "./components/portfolio/index";
-import Contact from "./components/contact/index";
 import Footer from "./components/footer/index"
 
 function App() { 
@@ -18,14 +14,8 @@ function App() {
 		<>
 			<Header/>
 			<Introduction/>
-			<Expertise/>
 			<Portfolio/>
-			<Education/>
-			<Bootcamps/>
 			<Skills/>
-			<Lenguages/>
-			<Profiles/>
-			<Contact/>
 			<Footer/>
 		</>
 	);
