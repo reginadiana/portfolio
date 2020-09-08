bd52e17b7190b256ba459cf015ec400df3e41861
diana.silva <diana.silva@rebase.com.br>
2020-09-08 20:33:56 -0300

    Build production

---
+++ build/precache-manifest.aa23553f3f447370d13fc4a44cad3107.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6da448b519fb9850edd24986ff890db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef6ff268b052020269b",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "f7ead324c6df983164cf",
+    "url": "/static/css/main.0918c3a6.chunk.css"
+  },
+  {
+    "revision": "fef6ff268b052020269b",
+    "url": "/static/js/2.00587b03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.00587b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ead324c6df983164cf",
+    "url": "/static/js/main.867b56f6.chunk.js"
+  },
+  {
+    "revision": "45063edbf8df3d8f39a0",
+    "url": "/static/js/runtime-main.7daa6418.js"
+  },
+  {
+    "revision": "b6bcb72f6443413c11b936ab56891f1b",
+    "url": "/static/media/amo_plantas.b6bcb72f.png"
+  },
+  {
+    "revision": "668d440984246c526de355cde034acfc",
+    "url": "/static/media/certificates_for_everyone.668d4409.png"
+  },
+  {
+    "revision": "09b641b6339802cb757aa89493a29fdc",
+    "url": "/static/media/ecommerce.09b641b6.png"
+  },
+  {
+    "revision": "cabf6adcadd75387f13f44d73c6628ff",
+    "url": "/static/media/headhunter.cabf6adc.png"
+  },
+  {
+    "revision": "1d1d678075ab2d27f46a4ec1bdc93560",
+    "url": "/static/media/rental_cars.1d1d6780.png"
+  },
+  {
+    "revision": "601509012c190a6c38b612be061d8690",
+    "url": "/static/media/skills.60150901.png"
+  },
+  {
+    "revision": "0392dbdbe8a5cf29db2762e7416f82c3",
+    "url": "/static/media/sobre_mim.0392dbdb.png"
+  },
+  {
+    "revision": "1bc911c04327c2c7ddbadb1d2e1d3ab1",
+    "url": "/static/media/wallpaper.1bc911c0.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css'
 import Header from "./components/header/index";
 import Introduction from "./components/introduction/index";
 import Skills from "./components/skills/index";
-import Education from "./components/education/index";
-import Bootcamps from "./components/bootcamps/index";
+//import Education from "./components/education/index";
+//import Bootcamps from "./components/bootcamps/index";
 import Portfolio from "./components/portfolio/index";
 import Footer from "./components/footer/index"
 
